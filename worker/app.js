aca87510e32f5e092cb559ac29d890e9cfbd0f3b
Sean Mai <seandmai@gmail.com>
2019-02-17 15:43:59 -0700

    Fixed some logic

--- worker/app.js
+++ worker/app.js
@@ -2,29 +2,28 @@ const puppeteer = require('puppeteer');
 
 (async function main(){
     try{
-        const browser = await puppeteer.launch({ headless: true });
+        const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
         page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36');
 
         //Repeated code to get sections.length for loop
         await page.goto('https://www.livetracklist.com/illenium-unreleased-2018-live-edits/');
         await page.waitForSelector('#TheList');
-        const tracklist = await page.$$('#TheList');
+        const tracklist = await page.$$('#TheList > li');
 
-        for(let i = 0; i < tracklist.children.length; i++) {
+        for(let i = 0; i < tracklist.length; i++) {
             // await page.goto('https://www.livetracklist.com/illenium-unreleased-2018-live-edits/');
             // await page.waitForSelector('#TheList');
             // const tracklist = await page.$$('#TheList');
 
-            const track = tracklist.children[i];
+            const track = tracklist[i];
             const trackString = track.innerText;
-            const trackTime = await track$('span .time-item');
-            const timeString = window.getComputedStyle(trackTime, ':before').content;
+            // const trackTime = await track$('span .time-item');
+            // const timeString = window.getComputedStyle(trackTime, ':before').content;
 
-            console.log(trackString);
-            console.log(timeString);
+            // console.log(trackString);
+            // console.log(timeString);
         }
-
     } catch(e){
         console.log("main async function error:", e);
     }
